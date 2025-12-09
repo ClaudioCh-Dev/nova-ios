f58094e26c5e140273f30d3294f0fc7e5abcb388
CarlosCT716 <I202402283@cibertec.edu.pe>
2025-12-08 23:41:50 -0500

    Login y registro listo

---
+++ NovaMovil/Views/RegisterViewController.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+
+
+
+class RegisterViewController: UIViewController {
+
+    
+    @IBOutlet weak var nombreView: UITextField!
+    
+    @IBOutlet weak var correoView: UITextField!
+    
+    @IBOutlet weak var telefonoView: UITextField!
+    
+    @IBOutlet weak var contraView: UITextField!
+    
+    @IBOutlet weak var contraConfir: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contraView.isSecureTextEntry = true
+        contraConfir.isSecureTextEntry = true
+
+    }
+ 
+
+    @IBAction func crearConfir(_ sender: Any) {
+    
+    guard let nombre = nombreView.text, !nombre.isEmpty,
+          let email = correoView.text, !email.isEmpty,
+          let telefono = telefonoView.text, !telefono.isEmpty,
+          let password = contraView.text, !password.isEmpty,
+          let confirm = contraConfir.text, !confirm.isEmpty else {
+        mostrarAlerta(mensaje: "Complete todos los campos")
+        return
+    }
+    
+    guard password == confirm else {
+        mostrarAlerta(mensaje: "Las contraseñas no coinciden")
+        return
+    }
+    
+    let nuevoUsuario = RegisterRequest(
+        fullName: nombre,
+        email: email,
+        phone: telefono,
+        password: password
+    )
+    
+    
+    UserService.shared.registrarUsuario(datos: nuevoUsuario) { [weak self] result in
+        DispatchQueue.main.async {
+            
+            switch result {
+            case .success(_):
+                self?.mostrarAlerta(mensaje: "Cuenta creada correctamente") {
+                    self?.navigationController?.popViewController(animated: true)
+                }
+                
+            case .failure(let error):
+                self?.mostrarAlerta(mensaje: "Error al registrar: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+private func mostrarAlerta(mensaje: String, completion: (() -> Void)? = nil) {
+    let alerta = UIAlertController(title: "Atención", message: mensaje, preferredStyle: .alert)
+    let accion = UIAlertAction(title: "Aceptar", style: .default) { _ in
+        completion?()
+    }
+    alerta.addAction(accion)
+    present(alerta, animated: true)
+}
+}
